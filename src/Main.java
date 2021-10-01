5cffd9d9e22dea8ba6a65c604fce6c8f708bb72d
22106463 <dhia-eddine.ammar@ens.uvsq.fr>
2021-10-01 15:34:57 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(" fraction : ");
+        Fraction a = new Fraction(1,2);
+        System.out.println(a);
+    }
+}
